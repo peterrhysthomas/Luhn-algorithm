8d1d6370bd1829dd67d0de7299a6f3046deca57a
peterrhysthomas@yahoo.co.uk <peterrhysthomas@yahoo.co.uk>
2015-11-19 23:11:26 +0000

    Removed unused steps

--- src/test/java/luhn/LuhnTestStepdefs.java
+++ src/test/java/luhn/LuhnTestStepdefs.java
@@ -18,24 +18,9 @@ public class LuhnTestStepdefs {
 		validator = new LuhnValidationController();
 	}
 
-	@When("^I verify a valid credit card number not containing spaces$")
-	public void I_verify_a_valid_credit_card_number_not_containing_spaces() {
-      creditCardNumber = "5420113841119649";
-	}
-
 	@Then("^the validation succeeds$")
 	public void the_validation_succeeds() throws ValidationFailureException {
 		validator.validate(creditCardNumber);
-	}
-
-	@When("^I verify a valid credit card number containing spaces$")
-	public void I_verify_a_valid_credit_card_number_containing_spaces() {
-		creditCardNumber = "5420 1138 4111 9649";
-	}
-
-	@When("^I verify a invalid credit card number$")
-	public void I_verify_a_invalid_credit_card_number() throws ValidationFailureException {
-		creditCardNumber = "99927398716";
 	}
 
 	@Then("^the validation fails$")
@@ -47,11 +32,6 @@ public class LuhnTestStepdefs {
 			//expected
 			//TODO - refactor this with cucumber syntax
 		}
-	}
-	
-	@When("^I verify a credit card number containing alphanumeric characters$")
-	public void I_verify_a_credit_card_number_containing_alphanumeric_characters() {
-		creditCardNumber = "5420 1138X4111A9649";
 	}
 	
 	@When("^I verify a blank credit card number$")
